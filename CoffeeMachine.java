6759fa399c9ac42ed2811223bcb7a45f5545aeca
RPrikk <rprikk98@gmail.com>
2021-08-09 15:58:12 +0300

    added ingredients calculator

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,14 +1,15 @@
 package machine;
-
+import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
 
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        int c = scanner.nextInt();
+
+        System.out.println("For " + c + " cups of coffee you will need:");
+        System.out.println(c * 200 + " ml of water");
+        System.out.println(c * 50 + " ml of milk");
+        System.out.println(c * 15 + " g of coffee beans");
     }
 }
