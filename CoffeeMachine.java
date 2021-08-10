80a18e057a3a871b70b400320468612eb7a183e3
RPrikk <rprikk98@gmail.com>
2021-08-10 12:45:31 +0300

    Extended ingredients calculator functionality with an else if loop

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,15 +1,35 @@
 package machine;
+
 import java.util.Scanner;
+
 public class CoffeeMachine {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int mlWater = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int mlMilk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int grCoffee = scanner.nextInt();
+
         System.out.println("Write how many cups of coffee you will need:");
+        int cupsNumber = scanner.nextInt();
 
-        int c = scanner.nextInt();
+        int cupsWater = mlWater / 200;
+        int cupsMilk = mlMilk / 50;
+        int cupsCoffee = grCoffee / 15;
+        int maxCups = Math.min(cupsCoffee, Math.min(cupsWater, cupsMilk));
 
-        System.out.println("For " + c + " cups of coffee you will need:");
-        System.out.println(c * 200 + " ml of water");
-        System.out.println(c * 50 + " ml of milk");
-        System.out.println(c * 15 + " g of coffee beans");
+        if ((cupsNumber * 200 > mlWater) || (cupsNumber * 50 > mlMilk) || (cupsNumber * 15 > grCoffee)) {
+            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
+        } else if (cupsNumber == maxCups) {
+            System.out.println("Yes, I can make that amount of coffee ");
+        } else {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (maxCups - cupsNumber) + " more than that)");
+        }
     }
 }
